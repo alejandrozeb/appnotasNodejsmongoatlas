358549b033c1d3c78bfb7301c367e83e9eef6105
unknown <alejandrojzeballos@gmail.com>
2020-12-19 02:20:43 -0400

    conectando con mongo atlas

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -1,4 +1,32 @@
 /* mongo db atlas
+registro con correo
+opcion gratuita
+Las estruvutras de mongodb son aws cloud y azure, mongo puede usar estas infraestructuras.
+aws
+region: escogemos la menor latencia
+descripcion del cluster
+podemos tener backups y configurar accesos
+created cluster
+pasos para iniciar un cluster
+configurar el mismo
+crear un cluster
+configurar la direccion ip para el acceso de base de datos
+cargar daots generados
+conectarnos a la bd
+mlab 
+creamos un nuevo usuario en database access
+alejandro-alejandro3
+configuramos la ip permitidas en network access
+no colocamos la ip de nuestro computador
+si no del servidor que usaremos para alojar la aplicacion
+por motivos de desarrollo y aprendizaje permitiremos a todas las ip.
 
+podemos conectarnos por 
+shell   de mongo en nuestra pc
+aplicacion  
+compass como phpmyadmin
+en la opcion shell te pide que tengas instalado mongo en tu computador.
+mongo "mongodb+srv://cluster0.7xxfx.mongodb.net/<dbname>" --username alejandro
+cambiamos por nustras credenciales
 
 */
